a1e12b742139da2862b57876761695094b0b0e70
Duong Dzoan <doanthanhduong11031990@gmail.com>
2020-12-18 11:02:56 +0700

    init code

---
+++ main.py
@@ -0,0 +1,22 @@
+# First time
+# create virual enviroment
+# py -3 -m venv .venv_basic_python
+# Ctrl + Shift + P -> Python: Select -> .venv_basic_python
+# Ctrl + Shift + P -> Terminal: Create New Integrated Terminal
+# Error cannot be loaded because running scripts is disabled on this system. For more information, see about_Execution_Policies
+# Change to Powershell: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+# pip install numpy
+
+# Everytime open project
+# Ctrl + Shift + P -> Terminal: Create New Integrated Terminal
+
+
+# Git
+#    Open the command line.
+#    Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
+#    Set your email address: git config --global user.email "MY_NAME@example.com"
+import numpy
+
+msg = "Hello world!!"
+
+print(msg)
